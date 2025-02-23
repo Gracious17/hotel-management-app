d3fb9eab28bf40d85bb315d6bd6e8f3281958468
Samuel-Omobuwa <samuelomobuwa1@gmail.com>
2025-02-23 19:25:50 +0100

    heroimage

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 
+
 const Hero = () => {
   return (
-    <div className=" bg-hero-img bg-center  bg-cover w-[100w] h-[80vh] ">
+    <div className=" flex items-center justify-center bg-cover bg-center w-[100w] h-[80vh]" style={{backgroundImage: "url('/hero.png')"}}>
       <div className="pt-10">
+       
         <h1 className="text-center font-bold  tracking-wide text-lg sm:text-xl md:text-2xl lg:text-2xl xl:text-3xl leading-10">
           Welcome Home!, We Deliver Experience <br />
           Make Your Memories
